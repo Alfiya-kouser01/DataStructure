23e12d7448b65620c00b1f3df4b47c831a3b51b0
Alfiya Kouser <alfiyakouser01@gmail.com>
2023-02-12 18:43:52 +0530

    adding recursion

--- src/recursion/sumOfN.java
+++ src/recursion/sumOfN.java
@@ -3,16 +3,16 @@ package recursion;
 public class sumOfN {
     static int sum=0;
     public static int addUp(int n){
-        sum=sum+n;
-        if(n!=0) {
-
-            addUp(n - 1);
-
-        }return sum;
+       // sum=sum+n;
+        if(n==0)
+            return 0;
+        return n+addUp(n-1);
 
     }
     public static void main(String[] args) {
-        int n=5;
+        int n=50;
         System.out.println(addUp(n));
+        int sum2 =(n*(n+1))/2;
+        System.out.println(sum2);
     }
 }
